5d89be3b698cdee653af90cbce7d5453f9144734
Harrison Balogh <harrisonbalogh@yahoo.com>
2017-10-10 16:05:01 -0700

    First Commit Default Project
    • Ignoring Tests

---
+++ Messenger478/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Messenger478
+//
+//  Created by Harrison Balogh on 10/10/17.
+//  Copyright © 2017 Harxer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Messenger478/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Messenger478
+//
+//  Created by Harrison Balogh on 10/10/17.
+//  Copyright © 2017 Harxer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
